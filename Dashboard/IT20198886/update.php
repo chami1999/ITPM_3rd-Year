9f840719de044bf2db6ab4e661cfdacf0e8a0a51
Chamini Sandunika <csvidanagamage@gmail.com>
2022-05-08 10:57:23 +0530

    n

--- Dashboard/IT20198886/update.php
+++ Dashboard/IT20198886/update.php
@@ -2,7 +2,7 @@
 include_once("dbconnect.php");
 if(isset($_POST['btn4']))
 {
-  $id = $_POST['user_id'];
+  $user_id = $_POST['user_id'];
       $name = $_POST['name'];
       $age = $_POST['age'];
       $gender = $_POST['gender'];
@@ -10,16 +10,22 @@ if(isset($_POST['btn4']))
       $dose = $_POST['dose'];
       $vaccination_date = $_POST['vaccination_date'];
 
-      mysqli_query($conn,"UPDATE vaccination_details set id='" . $_POST['user_id'] . "' ,name='" . $_POST['name'] . "',age='" . $_POST['age'] . "',gender='" . $_POST['gender'] . "',vaccine_type='" . $_POST['vaccinetype'] . "',dose='" . $_POST['dose'] . "',vaccination_date='" . $_POST['vaccination_date'] . "' WHERE user_Id='" . $_POST['user_id'] . "'");
+      if($con->connect_error){
+        die('connection faild :'.$con->connect_error);
+    }else{
+        $sql = "UPDATE vaccination_details SET ='$user_id', vaccine_type='$vaccine_type', quantity='$quantity',
+         store_date='$store_date', expire_date='$expire_date' 
+        WHERE id='$id'";
 
-if(mysqli_query($conn, $sql)){
-   
-    header("Location: ViewAssignDetails.php");
-} else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
-}
+        $rs = mysqli_query($con, $sql);
+        if($rs){
+            echo '<script>alert("Updated")</script>
+             <script>location.replace("Update_Vaccine_Details.php");</script>
+            ';
+        }
 
-
+        $con->close();
   
+}
 }
 ?>
